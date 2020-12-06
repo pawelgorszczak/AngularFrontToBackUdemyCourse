96441ad5a01b748a575c92f455f277850f83264d
pawelgorszczakpl <pawelgorszczakpl@gmail.com>
2020-12-06 19:59:55 +0100

    Prepare ui

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-form.component.scss']
 })
 export class LogFormComponent implements OnInit {
-
+  text: string = "test";
   constructor() { }
 
   ngOnInit(): void {
