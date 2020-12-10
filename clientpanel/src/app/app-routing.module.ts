ef1f0568268bede63135c450b6430acbee32be2e
pawelgorszczakpl <pawelgorszczakpl@gmail.com>
2020-12-10 17:29:45 +0100

    Clients dashboard

---
+++ clientpanel/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+
+
+const routes: Routes = [
+  {path:'', component: DashboardComponent},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'client/add', component: AddClientComponent},
+  {path:'client/edit/:id', component: EditClientComponent},
+  {path:'client/:id', component: ClientDetailsComponent},
+  {path:'settings', component: SettingsComponent},
+  {path:'**', component: NotFoundComponent}
+];
+
+@NgModule({
+  exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(routes)
+  ]
+})
+export class AppRoutingModule { }
---
+++ clientpanel/src/app/services/client.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable, of} from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Client } from '../models/Client'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+  clientsCollection: AngularFirestoreCollection<Client>;
+  clientDoc?: AngularFirestoreDocument<Client>;
+  clients: Observable<Client[]> = of([]);
+  client: Observable<Client> = of({});
+
+  constructor(private afs: AngularFirestore) { 
+    this.clientsCollection = afs.collection('clients', ref => ref.orderBy('lastName', 'asc'))
+  }
+
+  getClients(): Observable<Client[]> {
+    // get cliients with the id
+    this.clients = this.clientsCollection.snapshotChanges()
+    .pipe(
+      map((changes: any[]) => {
+        return changes.map(action => {
+          const data = action.payload.doc.data() as Client;
+          data.id = action.payload.doc.id;
+          return data;
+        })
+      })
+    );
+
+    return this.clients;
+  }
+}
