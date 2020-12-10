ab8327e94ca115ceae0da2ecd519fe63b19741ea
pawelgorszczakpl <pawelgorszczakpl@gmail.com>
2020-12-10 17:11:45 +0100

    Use firebase consumming framework

--- clientpanel/src/environments/environment.prod.ts
+++ clientpanel/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAdHjsBixI4YGBSq12Gw3cuT6vAZrQ3YzU",
+    authDomain: "clientpanelprod-50ad5.firebaseapp.com",
+    projectId: "clientpanelprod-50ad5",
+    storageBucket: "clientpanelprod-50ad5.appspot.com",
+    messagingSenderId: "560901470313",
+    appId: "1:560901470313:web:f409cf7958dcd86328caf8",
+    measurementId: "G-46W48SBSF9"
+  }
 };
--- clientpanel/src/environments/environment.ts
+++ clientpanel/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAdHjsBixI4YGBSq12Gw3cuT6vAZrQ3YzU",
+    authDomain: "clientpanelprod-50ad5.firebaseapp.com",
+    projectId: "clientpanelprod-50ad5",
+    storageBucket: "clientpanelprod-50ad5.appspot.com",
+    messagingSenderId: "560901470313",
+    appId: "1:560901470313:web:f409cf7958dcd86328caf8",
+    measurementId: "G-46W48SBSF9"
+  }
 };
 
 /*
