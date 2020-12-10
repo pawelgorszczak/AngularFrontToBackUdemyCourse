0472d4c6b97c1f7fa2a789e3c5f2c90ba79b7406
pawelgorszczakpl <pawelgorszczakpl@gmail.com>
2020-12-10 21:57:19 +0100

    Client details view

--- clientpanel/src/app/components/client-details/client-details.component.ts
+++ clientpanel/src/app/components/client-details/client-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Client } from 'src/app/models/Client';
+import { ClientService } from 'src/app/services/client.service';
 
 @Component({
   selector: 'app-client-details',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-details.component.scss']
 })
 export class ClientDetailsComponent implements OnInit {
+  id: string = '';
+  client?: Client;
+  hasBalance: boolean = true;
+  showBalanceUpdateInput: boolean = false;
 
-  constructor() { }
+  constructor(private clientService: ClientService, private router: Router, private route: ActivatedRoute, private flashMessage: FlashMessagesService) { }
 
   ngOnInit(): void {
+    // Get id from url
+    this.id = this.route.snapshot.params['id'];
+
+    // Get client
+    this.clientService.getClient(this.id).subscribe(client => {
+      if (client) {
+        if ((client.balance || 0) > 0) {
+          this.hasBalance = true;
+        }
+        this.client = client  
+      }
+    });
   }
 
+  onDelete(): void {
+    
+  }
 }
--- clientpanel/src/app/services/client.service.ts
+++ clientpanel/src/app/services/client.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
-import { Observable, of} from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Client } from '../models/Client'
 
@@ -11,9 +11,9 @@ export class ClientService {
   clientsCollection: AngularFirestoreCollection<Client>;
   clientDoc?: AngularFirestoreDocument<Client>;
   clients: Observable<Client[]> = of([]);
-  client: Observable<Client> = of({});
+  client: Observable<Client | null> = of({});
 
-  constructor(private afs: AngularFirestore) { 
+  constructor(private afs: AngularFirestore) {
     this.clientsCollection = afs.collection('clients', ref => ref.orderBy('lastName', 'asc'))
   }
 
@@ -24,16 +24,33 @@ export class ClientService {
   getClients(): Observable<Client[]> {
     // get cliients with the id
     this.clients = this.clientsCollection.snapshotChanges()
-    .pipe(
-      map((changes: any[]) => {
-        return changes.map(action => {
-          const data = action.payload.doc.data() as Client;
-          data.id = action.payload.doc.id;
-          return data;
+      .pipe(
+        map((changes: any[]) => {
+          return changes.map(action => {
+            const data = action.payload.doc.data() as Client;
+            data.id = action.payload.doc.id;
+            return data;
+          })
         })
-      })
-    );
+      );
 
     return this.clients;
+  }
+
+  getClient(id: string): Observable<Client | null> {
+    this.clientDoc = this.afs.doc<Client>(`clients/${id}`);
+    this.client = this.clientDoc.snapshotChanges()
+      .pipe(
+        map((action: any) => {
+          if (action.payload.exists === false) {            
+            return null;
+          }
+          const data = action.payload.data() as Client;
+          data.id = action.payload.id;
+          return data;
+        })
+      );
+
+    return this.client;
   }
 }
