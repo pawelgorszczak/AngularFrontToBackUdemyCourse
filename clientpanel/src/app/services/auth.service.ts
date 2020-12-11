419fed0a31315bc4c97c2649cce89eb5aece22ec
pawelgorszczakpl <pawelgorszczakpl@gmail.com>
2020-12-11 21:00:26 +0100

    fix guards

---
+++ clientpanel/src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { User } from 'firebase';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private afAuth: AngularFireAuth) { }
+
+  login(email: string, passowrd: string) : Promise<any>{
+    return new Promise((resolve, reject) => {
+      return this.afAuth.auth.signInWithEmailAndPassword(email, passowrd)
+      .then(userData => resolve(userData), err => reject(err));
+    });    
+  }
+
+  getAuth() : Observable<User | null> {
+    return this.afAuth.authState;
+  }
+
+  logout(): void {
+    this.afAuth.auth.signOut();
+  }
+}
