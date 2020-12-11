3ed3222f077a5e7883e6af4c8a219f96d5ec1136
pawelgorszczakpl <pawelgorszczakpl@gmail.com>
2020-12-11 20:34:55 +0100

    Add firebase auth

--- clientpanel/src/app/components/login/login.component.ts
+++ clientpanel/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  email: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router, private flashMessagesService: FlashMessagesService) { }
 
   ngOnInit(): void {
+    this.authService.getAuth().subscribe(auth => {
+      if (auth) {
+        this.router.navigate(['/']);
+      }
+    })
   }
 
+  onSubmit(): void {
+    this.authService.login(this.email, this.password)
+    .then(res => {
+      this.flashMessagesService.show('You are now logged in', {cssClass: 'alert-success', timeout: 4000});
+      this.router.navigate(['/']);
+    })
+    .catch(err => {
+      this.flashMessagesService.show(err, {cssClass: 'alert-danger', timeout: 4000});
+    });
+  }
 }
