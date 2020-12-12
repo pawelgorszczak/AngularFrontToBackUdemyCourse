d854f28fa574f11800922a88484beb66f103edc5
pawelgorszczakpl <pawelgorszczakpl@gmail.com>
2020-12-12 15:54:30 +0100

    add register page implementation

--- clientpanel/src/app/components/register/register.component.ts
+++ clientpanel/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  email: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router, private flashMessagesService: FlashMessagesService) { }
 
   ngOnInit(): void {
+    // this.authService.getAuth().subscribe(auth => {
+    //   if (auth) {
+    //     this.router.navigate(['/']);
+    //   }
+    // })
   }
 
+  onSubmit(): void {
+    this.authService.register(this.email, this.password)
+    .then(res => {
+      this.flashMessagesService.show('You are now registered and logged in', {cssClass: 'alert-success', timeout: 4000});
+      this.router.navigate(['/']);
+    })
+    .catch(err => {
+      this.flashMessagesService.show(err, {cssClass: 'alert-danger', timeout: 4000});
+    });
+  }
 }
--- clientpanel/src/app/services/auth.service.ts
+++ clientpanel/src/app/services/auth.service.ts
@@ -6,13 +6,19 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthService { 
 
   constructor(private afAuth: AngularFireAuth) { }
 
   login(email: string, passowrd: string) : Promise<any>{
     return new Promise((resolve, reject) => {
       return this.afAuth.auth.signInWithEmailAndPassword(email, passowrd)
+      .then(userData => resolve(userData), err => reject(err));
+    });    
+  }
+  register(email: string, password: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
       .then(userData => resolve(userData), err => reject(err));
     });    
   }
@@ -23,5 +29,5 @@ export class AuthService {
 
   logout(): void {
     this.afAuth.auth.signOut();
-  }
+  }  
 }
